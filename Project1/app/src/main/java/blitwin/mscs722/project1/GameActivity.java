0209c25176f9d124f37af76078eda93807e96cb3
brandonlitwin <brandon.litwin1@marist.edu>
2019-09-26 18:15:11 -0400

    Initial setup of game with title screen

---
+++ Project1/app/src/main/java/blitwin/mscs722/project1/GameActivity.java
@@ -0,0 +1,14 @@
+package blitwin.mscs722.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
---
+++ Project1/app/src/main/java/blitwin/mscs722/project1/MainActivity.java
@@ -0,0 +1,22 @@
+package blitwin.mscs722.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchGame(View view) {
+        Intent gameIntent = new Intent(this, GameActivity.class);
+        startActivity(gameIntent);
+    }
+
+}
