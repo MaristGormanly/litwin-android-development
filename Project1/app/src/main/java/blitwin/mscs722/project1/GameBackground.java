b735226005b1947ccd35fefeec8ea4b27ead6a17
brandonlitwin <brandon.litwin1@marist.edu>
2019-10-03 13:48:11 -0400

    Added game background

--- Project1/app/src/main/java/blitwin/mscs722/project1/GameBackground.java
+++ Project1/app/src/main/java/blitwin/mscs722/project1/GameBackground.java
@@ -3,14 +3,26 @@ package blitwin.mscs722.project1;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 
 public class GameBackground {
     private Bitmap space_bitmap;
     private Bitmap stars_bitmap;
+    private Bitmap combined_bitmap;
 
     public GameBackground(Context context) {
         space_bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.parallax_space_background);
         stars_bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.parallax_space_stars);
 
+        combined_bitmap = Bitmap.createBitmap(space_bitmap.getWidth(), space_bitmap.getHeight(), space_bitmap.getConfig());
+        Canvas canvas = new Canvas(combined_bitmap);
+        canvas.drawBitmap(space_bitmap, new Matrix(), null);
+        canvas.drawBitmap(stars_bitmap, 0, 0, null);
+
+    }
+
+    public Bitmap getBitmap() {
+        return combined_bitmap;
     }
 }
--- Project1/app/src/main/java/blitwin/mscs722/project1/GameView.java
+++ Project1/app/src/main/java/blitwin/mscs722/project1/GameView.java
@@ -1,19 +1,30 @@
 package blitwin.mscs722.project1;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.DisplayMetrics;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
 public class GameView extends SurfaceView implements Runnable {
 
     volatile boolean isPlaying;
 
+    private ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
     private Thread gameThread = null;
 
     private PlayerShip player;
+    private GameBackground gameBG;
+    //private Bitmap gameBGBitmap = gameBG.getBitmap();
+
+    private int screenHeight;
+    private int screenWidth;
 
     private Paint paint;
     private Canvas canvas;
@@ -24,6 +35,14 @@ public class GameView extends SurfaceView implements Runnable {
 
         // initialize player
         player = new PlayerShip(context);
+
+        // initialize background
+        gameBG =  new GameBackground(context);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
 
         surfaceHolder = getHolder();
         paint = new Paint();
@@ -46,11 +65,11 @@ public class GameView extends SurfaceView implements Runnable {
     private void draw() {
         if (surfaceHolder.getSurface().isValid()) {
             canvas = surfaceHolder.lockCanvas();
-            canvas.drawColor(Color.BLACK);
-            canvas.drawBitmap();
-            canvas.drawBitmap(
-                    player.getBitmap(), player.getX(), player.getY(), paint
-            );
+            // draw background
+            Bitmap resized_background = Bitmap.createScaledBitmap(gameBG.getBitmap(), screenWidth, screenHeight, true);
+            canvas.drawBitmap(resized_background, 0, 0, null);
+            // draw player bitmap
+            canvas.drawBitmap(player.getBitmap(), player.getX(), player.getY(), paint);
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
     }
