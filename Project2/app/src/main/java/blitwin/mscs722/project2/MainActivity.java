0eadfb8df7462bd62fd74e1e1e70ade1eeb4da6e
Brandon Litwin <brandon.litwin1@marist.edu>
2019-10-28 17:12:47 -0400

    Change speed of individual sounds

--- Project2/app/src/main/java/blitwin/mscs722/project2/MainActivity.java
+++ Project2/app/src/main/java/blitwin/mscs722/project2/MainActivity.java
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.AudioAttributes;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private SoundPool soundPool;
     private int cgs, st, ds;
-//    Button ds;
-//    Button cgs;
-//    Button st;
     int maxStreams = 3;
-    float soundRate = 1;
+    float cgsSoundRate = 1;
+    float dsSoundRate = 1;
+    float stSoundRate = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,33 +46,40 @@ public class MainActivity extends AppCompatActivity {
         st = soundPool.load(this, R.raw.stillthere, 1);
         ds = soundPool.load(this, R.raw.damnson, 1);
 
+        Spinner spinner = findViewById(R.id.cgsSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.playback_choices, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(1);
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner2 = findViewById(R.id.stSpinner);
+        spinner2.setAdapter(adapter);
+        spinner2.setSelection(1);
+        spinner2.setOnItemSelectedListener(this);
+
+        Spinner spinner3 = findViewById(R.id.dsSpinner);
+        spinner3.setAdapter(adapter);
+        spinner3.setSelection(1);
+        spinner3.setOnItemSelectedListener(this);
+
     }
 
     public void playSound(View view) {
         // Play different sound depending on which button is pressed
         switch (view.getId()) {
             case R.id.come_get_some:
-                soundPool.play(cgs, 1, 1, 0, 0, soundRate);
+                soundPool.play(cgs, 1, 1, 0, 0, cgsSoundRate);
                 break;
             case R.id.damn_son:
-                soundPool.play(ds, 1, 1, 0, 0, soundRate);
+                soundPool.play(ds, 1, 1, 0, 0, dsSoundRate);
                 break;
             case R.id.still_there:
-                soundPool.play(st, 1, 1, 0, 0, soundRate);
+                soundPool.play(st, 1, 1, 0, 0, stSoundRate);
                 break;
         }
-    }
-
-    public void speed2X(View view) {
-        soundRate = 2;
-    }
-
-    public void speed1X(View view) {
-        soundRate = 1;
-    }
-
-    public void speed05X(View view) {
-        soundRate = (float) 0.5;
     }
 
     @Override
@@ -81,4 +90,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        float soundRate = 1;
+        Log.d("position", Integer.toString(position));
+        switch (position) {
+            case 0:
+                soundRate = (float) 0.5;
+                break;
+            case 1:
+                soundRate = 1;
+                break;
+            case 2:
+                soundRate = 2;
+        }
+        switch (parent.getId()) {
+            case R.id.cgsSpinner:
+                cgsSoundRate = soundRate;
+                break;
+            case R.id.stSpinner:
+                stSoundRate =  soundRate;
+                break;
+            case R.id.dsSpinner:
+                dsSoundRate = soundRate;
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
