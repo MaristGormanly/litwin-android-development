9d56b382fd986a32a7703266c8a2da6d81d26fe5
Brandon Litwin <brandon.litwin1@marist.edu>
2019-11-01 14:36:37 -0400

    Uploading a file to firebase storage

--- Project2/app/src/main/java/blitwin/mscs722/project2/MainActivity.java
+++ Project2/app/src/main/java/blitwin/mscs722/project2/MainActivity.java
@@ -1,24 +1,98 @@
 package blitwin.mscs722.project2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button uploadBtn;
+    private Uri filePath;
+    private TextView filePathText;
+
+    private final int SELECT_AUDIO = 71;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        filePathText = findViewById(R.id.filePathText);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        /*uploadBtn = findViewById(R.id.upload);
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
+
     }
 
     public void start(View view) {
         Intent gameIntent = new Intent(this, GameActivity.class);
         startActivity(gameIntent);
+    }
+
+    public void chooseFile(View view) {
+        Intent intent = new Intent();
+        intent.setType("audio/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_AUDIO);
+
+    }
+
+    public void uploadFile(View view) {
+        if (filePath != null) {
+            StorageReference ref = storageReference.child("audio/" + UUID.randomUUID().toString());
+            ref.putFile(filePath).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed "+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_AUDIO && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            filePathText.setText(filePath.toString());
+
+        }
     }
 
 }
